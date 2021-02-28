cbf393f78787c35032aa8263e22212a8e5d98807
Arely <Jairo@LAPTOP-TE9AK6EP>
2021-02-27 18:44:52 -0600

    primer commit

---
+++ src/tipodedatos60223/TipoDeDatos60223.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipodedatos60223;
+
+/**
+ *
+ * @author Jairo
+ */
+public class TipoDeDatos60223 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         
+         
+    byte mínByte = Byte.MIN_VALUE;
+    byte máxByte = Byte.MAX_VALUE;
+    
+    System.out.println("Byte mínimo: " + mínByte);
+    System.out.println("Byte máximo: " + máxByte);
+    
+    short mínShort = Short.MIN_VALUE;
+    short máxShort = Short.MAX_VALUE;
+    
+    System.out.println("Short mínimo: " + mínShort);
+    System.out.println("Short máximo: " + máxShort);
+    
+    int mínInt = Integer.MIN_VALUE;
+    int máxInt = Integer.MAX_VALUE;
+    
+    System.out.println("Int mínimo: " + mínInt);
+    System.out.println("Int máximo: " + máxInt);
+    
+    long mínLong = Long.MIN_VALUE;
+    long máxLong = Long.MAX_VALUE;    
+            
+     System.out.println("Long mínimo: " + mínLong);
+     System.out.println("Long máximo: "+ máxLong);
+    
+    double mínDouble = Double.MIN_VALUE;
+    double máxDouble = Double.MAX_VALUE;   
+    
+            
+    System.out.println("Double mínimo: " + mínDouble);
+    System.out.println("Double máximo: " + máxDouble);}
+    
+                 
+
+           
+           
+    }
+    
